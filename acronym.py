e373a619ad333ff3a9dfcab7f7e820a906ae7f45
Jonathan Davis <jonnysdavis@gmail.com>
2018-09-13 05:31:28 -0400

    find acronyms and their locations

---
+++ acronym.py
@@ -0,0 +1,30 @@
+__author__ = 'Jonny'
+
+shorthand = ""
+expanded = ""
+cofidence = False
+
+
+class acronym:
+
+    def acronymLength():
+        return shorthand.len()
+
+def expandedLength():
+    return expanded.len()
+
+def acronym(shorthanded, expand):
+    shorthand = shorthanded
+    expanded = expand
+    confidence = True
+
+
+def acronym(isAcronym, text):
+    if(isAcronym == True):
+        shorthand = text
+        expanded = "?"
+    else:
+        expanded = text
+        shorthand = "convert to acronym"
+
+
